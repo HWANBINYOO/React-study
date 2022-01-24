bb17fe57a79d77a7ca8c6338ade5d1a25b9d1113
HAWNBINYOO <dngh0825@gmail.com>
2022-01-24 14:11:12 +0900

    use effect

--- react-for-begin/src/App.js
+++ react-for-begin/src/App.js
@@ -2,10 +2,28 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
+function Hello(){
+  function byFn(){
+    console.log("bye :(");
+  }
+
+  function hiFn(){
+    console.log("Created");
+    return byFn;
+  }
   
+  useEffect(hiFn, [])
+    return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev);
   return (
-    
+    <div>
+      {showing ? <Hello /> : null}
+        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
   );
 }
 
