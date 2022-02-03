65f4c88670cfad8a350daadc91c0fde41927b0bf
HAWNBINYOO <dngh0825@gmail.com>
2022-02-03 21:53:47 +0900

    sfdg

--- react-for-begin/src/App.js
+++ react-for-begin/src/App.js
@@ -1,28 +1,36 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-function Hello(){
-  function byFn(){
-    console.log("bye :(");
-  }
-
-  function hiFn(){
-    console.log("Created");
-    return byFn;
-  }
-  
-  useEffect(hiFn, [])
-    return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+  const [toDo, setToDo] = useState(""); //todo 입력
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+      </ul>
     </div>
   );
 }
