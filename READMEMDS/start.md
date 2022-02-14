3b1beca7c7a93ab3ad355005adc16882c0a0d937
HAWNBINYOO <dngh0825@naver.com>
2022-02-15 00:41:02 +0900

    study

--- READMEMDS/start.md
+++
@@ -1,3 +0,0 @@
-> yarn create react-app {프로젝트이름}
-> cd {프로젝트이름}
-> yarn start
--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <CSSModule />
       </div>
     );
   }
