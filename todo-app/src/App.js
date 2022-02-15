f0c0ace5c5c5a26128fd52610425a68c20b0f12e
HAWNBINYOO <dngh0825@naver.com>
2022-02-15 17:00:01 +0900

    study

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import TodoInsert from './components/Todolnsert';
+import TodoTemplate from './components/TodoTemplate';
+import { useState } from 'react';
 
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링하기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  );
 };
 
 export default App;
---
+++ todo-app/src/components/TodoListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+import './TodoListItem.scss';
+
+const TodolistItem = ({ todo }) => {
+  const { text, checked } = todo;
+
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+export default TodolistItem;
