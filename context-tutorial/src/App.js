2652aa92704e4476e309fce4809e3c8c43ebed89
HAWNBINYOO <dngh0825@naver.com>
2022-02-19 19:11:12 +0900

    2/19/19

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
 import { ColorProvider } from "./contexts/color";
+import SelectColors from "./components/SelectColors";
 
 function App() {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
