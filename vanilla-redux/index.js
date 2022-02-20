0770fe2228645394cac2fdf2623cfa7a53f3e1e5
HAWNBINYOO <dngh0825@naver.com>
2022-02-20 20:49:44 +0900

    2/20/20

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const divToggle = document.querySelector(".toggle");
 const counter = document.querySelector("h1");
 const btnIncrease = document.querySelector("#increase");
@@ -10,3 +12,59 @@ const DECREASE = "DECREASE";
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = (difference) => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+// state 가 undefined 일 떄는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type 에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+//스토어 만들기
+const store = createStore(reducer);
+//render라는 함수를 작성해 보겠습니다. 이 함수는 상태가 업데이트될 때마다 호출
+const render = () => {
+  const state = store.getState(); //현재 상태를 불러옵니다.
+  //토글 처리
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+  //카운터 처리
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
