fe72420c8238dbe976a8bad084d622b3783ea9b9
HAWNBINYOO <dngh0825@naver.com>
2022-02-21 23:36:03 +0900

    2/21/23

---
+++ react-redux-tutorial/src/modules/todos.js
@@ -0,0 +1,79 @@
+import Todos from '../components/Todos';
+
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
+const INSERT = 'INSERT'; // 새로운 todo를 등록함
+const TOGGLE = 'todos/TOGGLE'; //todo를 체크/체크 해제함
+const REMOVE = 'todos/REMOVE'; //todo를 제거함
+
+export const onChangeInput = (input) => ({
+  type: CHANGE_INPUT,
+  input,
+});
+
+let id = 3; //todo 객체 두 개를 사전에 미리 넣어 둘 것이다
+// insert가 호출될 때마다 id가 1씩 더해집니다.
+export const insert = (text) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false,
+  },
+});
+
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
+
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초 배우기',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스 사용하기',
+      done: false,
+    },
+  ],
+};
+
+function todos(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        inputa: action.input,
+      };
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(action.todo),
+      };
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+        ),
+      };
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+
+export default todos;
