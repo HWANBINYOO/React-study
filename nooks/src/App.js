5fefad2a56375600599a74220177683d2164a0a8
HAWNBINYOO <dngh0825@naver.com>
2022-02-24 08:39:12 +0900

    노마트코더 훅

---
+++ nooks/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello </h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+export default App;
