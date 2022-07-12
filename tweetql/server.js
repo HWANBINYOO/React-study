d5642da95c646e9917cbb27a1b6b4d0cd17b69d0
HAWNBINYOO <dngh0825@naver.com>
2022-07-12 12:15:19 +0900

    Query Type

---
+++ tweetql/server.js
@@ -0,0 +1,14 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
