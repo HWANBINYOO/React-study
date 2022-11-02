33a4e7b8da55339075e0779ee9659e72743ba422
hawnbin <dngh0825@naver.com>
2022-11-02 23:36:45 +0900

    Add Setup React redux

---
+++ vanilla-redux/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
+
+function App() {
+    return (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" exact element={<Home />}></Route>
+    <Route path="/:id" element={<Detail />}></Route>
+    </Routes>
+    </BrowserRouter>
+    );
+}
+
+export default App
