3c918e0f9eecfd8875840c1c18b6c5d7917bff00
hawnbin <dngh0825@naver.com>
2022-11-03 09:48:05 +0900

    Add Detail Page

--- vanilla-redux/src/components/ToDo.js
+++ vanilla-redux/src/components/ToDo.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteToDo } from "../store";
+import { Link } from "react-router-dom";
 
 
 function ToDo({ text, id }) {
     const dispatch = useDispatch();
+
     const onClick = () => {
         dispatch(deleteToDo(id));
         };    return (
     <li>
-      {text} <button onClick={onClick}>❌</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onClick}>❌</button>
+      </Link>
     </li>
   );
 }
--- vanilla-redux/src/routes/Detail.js
+++ vanilla-redux/src/routes/Detail.js
@@ -1,1 +1,19 @@
-export default () => "Detail";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+const myId = useParams().id;
+const toDos = useSelector((state)=>state);
+// find = 만족하는 첫번째 요소를 반환
+const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
+
+return (
+    <>
+    <h1>{toDo?.text}</h1> 
+    <h1>Created at: {toDo?.id}</h1>
+    </>
+);
+}
+
+export default Detail;
